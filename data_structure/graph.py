e05776e91477921e3a257ad380c64879704b1a87
Joel Jacob <joeljacobdev@gmail.com>
2023-03-11 21:21:21 +0530

    Add topological sorting

--- data_structure/graph.py
+++ data_structure/graph.py
@@ -1,4 +1,4 @@
-from collections import deque
+from collections import deque, defaultdict
 from typing import Dict
 
 
@@ -39,7 +39,7 @@ class Graph:
 
         return new_node
 
-    def dfs(self, root, node_map: Dict['Node'] = dict):
+    def dfs(self, root, node_map: Dict[int, Node] = dict):
         # check if a node is visited
         if root.val in node_map:
             return node_map[root.val]
@@ -58,3 +58,86 @@ class Graph:
         # node_map = {}
         # return self.dfs(node, node_map)
         return self.bfs(node)
+
+    @classmethod
+    def topological_sorting(cls, edges, node_no):
+        """
+        Implemented using BFS (Kahn's Algo)
+        Returns the topological order. Raises Exception in case there is cycle.
+        :param edges: list of "directed" edges in a graph (not necessary that all nodes in a graph are connected, ie. any edge to be present)
+            Edges are 0 index based.
+        :param node_no: Number of nodes
+        :return: list of nodes in topological order
+        """
+        # prepare adjacency list and compute in-degree
+        adj_graph = defaultdict(list)
+        in_degree = [0] * node_no
+        for edge in edges:
+            adj_graph[edge[0]].append(edge[1])
+            in_degree[edge[1]] += 1
+
+        queue = deque()
+        leaves = [idx for idx, val in enumerate(in_degree) if val == 0]
+        queue.extend(leaves)
+
+        topo_ordering = []
+
+        """
+        Remove leaves nodes one by one (and update in degree along). Leaves have in-degree == 0.
+        In case not all nodes processed - it contain cycle.
+        There is no need for visited, as we track the visit by setting in-degree to 0. 
+        And we add these node to queue whenever - we make then eligible (i.e no dependency for them) for processing
+        """
+        while queue:
+            node = queue.popleft()
+            topo_ordering.append(node)
+
+            for to in adj_graph[node]:
+                # reduce in degree due to node removal
+                in_degree[to] -= 1
+                # add those node to queue - which have become leaf
+                if in_degree[to] == 0:
+                    queue.append(to)
+
+        if len(topo_ordering) < node_no:
+            raise Exception('Graph contain cycle')
+        return topo_ordering
+
+    @classmethod
+    def topological_sorting_dfs(cls, edges, node_no):
+        """
+        Implemented using DFS
+        Returns the topological order
+        :param edges: list of "directed" edges in a graph (not necessary that all nodes in a graph are connected, ie. any edge to be present)
+            Edges are 0 index based.
+        :param node_no: Number of nodes
+        :return: list of nodes in topological order
+        """
+        # TODO: Can we detect cycle using this?
+        adj_graph = defaultdict(list)
+        for edge in edges:
+            adj_graph[edge[0]].append(edge[1])
+        visited = [False] * node_no
+
+        ans = []
+
+        """
+        We are inserting the nodes into the stack after the completion of the traversal, 
+        we are making sure, there will be no one who appears afterward but may come before in the
+        ordering as everyone during the traversal would have been inserted into the stack.
+        """
+        def dfs(n, visited):
+            visited[n] = True
+            for it in adj_graph[n]:
+                if not visited[it]:
+                    dfs(it, visited)
+            ans.append(n)
+
+        for node in range(node_no):
+            if not visited[node]:
+                dfs(node, visited)
+        ans.reverse()
+        return ans
+
+# Graph.topological_sorting(edges=[[3,0],[3,1],[3,2],[3,4],[5,4]], node_no=6)
+# Graph.topological_sorting_dfs(edges=[[3,0],[3,1],[3,2],[3,4],[5,4]], node_no=6)
